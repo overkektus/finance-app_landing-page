232a46a8f115caea0f7fd82d23060692f79634a9
Egor Piskunou <egor.piskunou@gmail.com>
2023-08-03 15:03:13 +0200

    install & setup fontawesome icons

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Header from '@/app/sections/Header/Header'
+import Main from '@/app/sections/Main/Main'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+// Tell Font Awesome to skip adding the CSS automatically
+// since it's already imported above
+config.autoAddCss = false
 
 export default function Home() {
   return (
     <div className="bg-color-primary text-color-white tracking-wider">
       <Header />
+      <Main />
     </div>
   )
 }
