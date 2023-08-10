dc7730f9bca7c6fdf0c0907a66be1407ff689ed7
Egor Piskunou <egor.piskunou@gmail.com>
2023-08-10 13:33:17 +0200

    add sections on page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,13 @@ import Header from '@/app/sections/Header/Header'
 import Main from '@/app/sections/Main/Main'
 import Features from '@/app/sections/Features/Features'
 import SavingMoney from '@/app/sections/SavingMoney/SavingMoney'
+import HowItWorks from '@/app/sections/HowItWorks/HowItWorks'
+import UserReviews from '@/app/sections/UserReviews/UserReviews'
+import Pricing from '@/app/sections/Pricing/Pricing'
+import Blog from '@/app/sections/Blog/Blog'
+import DownloadApp from '@/app/sections/DownloadApp/DownloadApp'
+import Contact from '@/app/sections/Contact/Contact'
+import Footer from '@/app/sections/Footer/Footer'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 // Tell Font Awesome to skip adding the CSS automatically
@@ -16,6 +23,13 @@ export default function Home() {
       <Main />
       <Features />
       <SavingMoney />
+      <HowItWorks />
+      <UserReviews />
+      <Pricing />
+      <Blog />
+      <DownloadApp />
+      <Contact />
+      <Footer />
     </div>
   )
 }
