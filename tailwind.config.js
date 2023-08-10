f2038848dc5def5690e4ebce090cb20de2f07512
Egor Piskunou <egor.piskunou@gmail.com>
2023-08-10 13:28:02 +0200

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
+const rotateY = plugin(function ({ addUtilities }) {
+  addUtilities({
+    '.rotate-y-180': {
+      transform: 'rotateY(180deg)',
+    },
+    '.-rotate-y-180': {
+      transform: 'rotateY(-180deg)',
+    },
+  })
+})
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -23,5 +37,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [rotateY],
 }
